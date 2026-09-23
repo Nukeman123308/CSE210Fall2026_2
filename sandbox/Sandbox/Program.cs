a96bd91e720554ae64aa511d28bba5c9520bcdf8
Benjamin Contreras <benjamincontreras336@@gmail.com>
2026-09-23 10:00:38 -0600

    Examples of loops

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour");
+    //     bool done = false; 
+
+    //     while (!done )
+
+    // {
+    //     Console.Write("Are we done? (y/n): ");
+    //     done = Console.ReadLine() == "y"; 
+   // }
+ 
+
+    // bool done; 
+
+    // do 
+    // {
+    //     Console.Write ("Are we done?(y/n): ");
+    //     done = Console.ReadLine().ToLower() == "y"; 
+    // } while(!done); 
+    
+
+    // for(int i = 0; i <=100; i+=5) 
+    //     {
+    //         Console.WriteLine($"{i}"); 
+    //         Console.WriteLine();
+    //         Console.WriteLine("Hey Bob!"); 
+    //     }
+
+    // List<string> myFriends = ["Bob", "Betty", "Bubba"]; 
+    // myFriends.Add("James"); 
+    // myFriends.Add("doug"); 
+
+    // foreach(string name in myFriends)
+    //     {
+    //         Console.WriteLine("name"); 
+    //     }
+
     }
+
 } 
